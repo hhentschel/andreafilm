4fdf473bd620b5dea395df539abb930467dbedfe
Henrik Hentschel <h21@protonmail.com>
2019-04-20 13:45:36 +0200

    css

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -8,18 +8,31 @@
 
 
 $(document).ready((function() {
-  var width = $(window).width(); 
-  var height=$(window).height(); 
-  var mydiv=$("#container__navbox-home");
-  mydiv.width(width);
-  mydiv.height(height);
 
-  $(window).resize((function(){
-    width = $(window).width();
-    height=$(window).height();
-    mydiv.width(width);
-    mydiv.height(height);
 
+  $('.container__navbox--arrow').click((function() {
+      $(this).hide();
   }));
 
+
+  $('a[href*="#"]').on('click',(function(e) {
+    e.preventDefault();
+    var target = this.hash;
+    var $target = $(target);
+    $('html, body').stop().animate({
+     'scrollTop': $target.offset().top
+    }, 900, 'swing', (function () {
+     window.location.hash = target;
+    }));
+   }));
+
+}));
+
+$(window).resize((function(){
+  if(window.innerHeight < 610){
+    $('.container__navbox').addClass('mobile');
+  }
+  else {
+    $('.container__navbox').removeClass('mobile');
+  }
 }));
