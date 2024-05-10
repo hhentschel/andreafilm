df5a3853cd4aef99d3915d9017c867c0767768d8
Henrik Hentschel <henrik.hentschel@zeix.com>
2024-05-10 12:56:58 +0200

    set up vite build process

---
+++ src/js/app.ts
@@ -0,0 +1,9 @@
+// import css
+import '~/sass/styles.scss'
+
+// Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+      console.log('HMR active')
+  })
+}
